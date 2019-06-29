79d1afcbd43681694fc9b53a1ba2dd66b43b8265
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-06-29 11:33:29 -0300

    finalizado

--- webcam.py
+++ webcam.py
@@ -18,10 +18,16 @@ while True:
 
     contours,_ = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    for (i, c) in enumerate(contours):
+    for (i, c) in enumerate(contours): # contorno
         (x, y, w, h) = cv2.boundingRect(c)
         area = cv2.contourArea(c)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cmY = (h*15)/120
+        cmX = (h*10)/130
+        cv2.putText(frame, str("%.2f" % cmY) + 'cm', (x, y+h+15), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,255, 255), 1)
+        cv2.putText(frame, str("%.2f" % cmX) + 'cm', (x+w+15, y+h+15), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,255, 255), 1)
+
+
 
     cv2.imshow("Frame", frame)
     # cv2.imshow("gray", gray)
